c400490c91c4f380574d0f20ba48eea6c4fd89d2
Alex <anicoara@hawk.iit.edu>
2017-12-01 15:01:32 -0600

    Adding variables to tic tac javascript program

---
+++ js/tictac.js
@@ -0,0 +1,15 @@
+$.noConflict();
+
+// Reintroduce the dolalr sign scope to self executing function
+(function($) {
+  // variables
+  var player;
+  var computer;
+  var hasWon;
+  var board;
+  var spaces_available;
+
+  // actions
+
+  // functions
+})(jQuery);
